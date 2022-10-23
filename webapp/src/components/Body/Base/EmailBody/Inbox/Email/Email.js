e4ea960e7a5554b8c18ad760cacf33149e236dcc
herbalvegetable <herbalvegetable@gmail.com>
2022-10-24 02:20:31 +0800

    update inbox

---
+++ webapp/src/components/Body/Base/EmailBody/Inbox/Email/Email.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import { Image } from 'react-bootstrap';
+
+import styles from './Email.module.css';
+
+const Email = props => {
+
+    const { author, title, body, unread } = props;
+
+    const [isHover, setIsHover] = useState(false);
+    const [isHoverDelete, setIsHoverDelete] = useState(false);
+
+    const [selected, setSelected] = useState(false);
+
+    return (
+        <div 
+            className={`${styles.main} ${unread ? styles.unread : ''}`}
+            onMouseOver={() => setIsHover(true)}
+            onMouseOut={() => setIsHover(false)}>
+            <div className={`${styles.profile_icon_container} ${unread ? styles.unread : ''}`}>
+            {
+                isHover ? 
+
+                <input type='radio' className={styles.radio_btn}/>
+
+                :
+
+                <div className={styles.profile_icon}>
+                    <span className={styles.text}>{author.slice(0, 2).toUpperCase()}</span>
+                </div>
+            }
+            </div>
+
+            <div className={styles.content}>
+                <div className={styles.top_row}>
+                    <div className={`${styles.author} ${unread ? styles.unread : ''}`}>{author}</div>
+                    {
+                        isHover && <div className={styles.actions}>
+                            <div className={styles.action}>
+                                <Image 
+                                    src='./media/inbox/mark_unread.png'
+                                    className={styles.img}/>
+                            </div>
+                            <div className={styles.action}>
+                                <Image 
+                                    src='./media/inbox/flag.png'
+                                    className={styles.img}/>
+                            </div>
+                            <div className={styles.action}>
+                                <Image 
+                                    src='./media/inbox/pin.png'
+                                    className={styles.img}/>
+                            </div>
+                        </div>
+                    }
+                </div>
+                <div className={styles.middle_row}>
+                    <div className={`${styles.title} ${unread ? styles.unread : ''}`}>{title}</div>
+                    <div className={`${styles.time} ${unread ? styles.unread : ''}`}>Sun 15:11</div>
+                </div>
+                <div className={`${styles.body} ${unread ? styles.unread : ''}`}>{body}</div>
+            </div>
+
+            <div className={styles.space_container}>
+                {
+                    isHover && <div 
+                        className={styles.delete_btn}
+                        onMouseOver={() => setIsHoverDelete(true)}
+                        onMouseOut={() => setIsHoverDelete(false)}>
+                        <Image 
+                            src={`./media/inbox/${isHoverDelete ? 'red_delete': 'delete'}.png`}
+                            className={styles.img}/>
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Email;
--- webapp/src/components/Body/Base/EmailBody/Inbox/Inbox.js
+++ webapp/src/components/Body/Base/EmailBody/Inbox/Inbox.js
@@ -1,14 +1,169 @@
 import { useState, useEffect } from 'react';
+import { Image } from 'react-bootstrap';
 
 import styles from './Inbox.module.css';
 
+import Email from './Email/Email';
+
 const Inbox = props => {
 
+    const [emails, setEmails] = useState([
+        {
+            author: 'u/Maximum_Afternoon_40',
+            title: '[O levels] urgent help for SS',
+            body: 'Yep I am failing SS 😭😭 idk every since the start of sec 4, I’ve always failed SS 🥲 despite memorising format for SBQ. I still end up failing cause I “misinterpret” the sources wrongly… any tips on it? Those around me always score quite high for their papers and honestly idek how they are doing it :,) There are so many bloody topics for SRQ so ig that’s the end of me for S',
+            unread: false,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        {
+            author: 'dabalabibala',
+            title: 'how do you memorise?',
+            body: 'hey guys, ive been rememorising all the chemistry content and i was curious how you fellers memorise? i usually write out or type out what im reading in order to memorise, what about you? good luck for os!',
+            unread: true,
+        },
+        
+    ]);
 
+    useEffect(() => {
+
+    }, []);
 
     return (
         <div className={styles.main}>
-            Inbox
+
+            <div className={styles.header}>
+                <div className={styles.checkmark_img_container}>
+                    <Image 
+                        src={'./media/inbox/checkmark.png'}
+                        className={styles.img}/>
+                </div>
+                <div className={`${styles.section} ${styles.focused}`}>
+                    <span className={styles.text}>Focused</span>
+                </div>
+                <div className={styles.section}>
+                    <span className={styles.text}>Other</span>
+                </div>
+                
+                <div className={styles.filter_container}>
+                    <div className={styles.img_container}>
+                        <Image 
+                            src={'./media/inbox/filter.png'}
+                            className={styles.img}/>
+                    </div>
+                    <span className={styles.text}>Filter</span>
+                </div>
+            </div>
+
+            <div className={styles.email_list}>
+            {
+                emails.map((email, i) => {
+                    return <Email 
+                        key={i.toString()}
+                        {...email}/>
+                })
+            }
+            </div>
         </div>
     )
 }
